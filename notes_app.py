461c8a62266fce002eae62be5f6dc43206955e0d
Dima <dmitryberesten@gmail.com>
2026-02-14 13:06:45 +0200

    додав клас та ініціалізацію

--- notes_app.py
+++ notes_app.py
@@ -1,2 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
+
+class NotesApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Командний менеджер нотаток")
+        self.root.geometry("400x500")
+        self.root.configure(bg="#f0f0f0")
